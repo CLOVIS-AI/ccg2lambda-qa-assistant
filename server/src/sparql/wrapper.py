fb269b5cf6ad3bb6fc89cba940830e7675278275
Carpilieng <gamargueritte@gmail.com>
2019-04-12 16:30:52 +0900

    working request with Wikidata

---
+++ server/src/sparql/wrapper.py
@@ -0,0 +1,28 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+# url where the query has to be sent through the wrapper
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+sparql.setQuery("""
+    #Les dix êtres humains les plus lourds
+    #defaultView:BubbleChart
+    #TEMPLATE={ "template": "The top 10 heaviest ?type ", "variables": { "?type": { "query": "SELECT DISTINCT ?id WHERE { ?i wdt:P2067 ?v. ?i wdt:P31 ?id}" } } }
+    SELECT ?item ?itemLabel ?mass
+    WHERE {
+    {
+    SELECT ?item ?mass WHERE {
+      ?item wdt:P31 wd:Q5;
+            p:P2067/psn:P2067/wikibase:quantityAmount ?mass.
+    }
+    ORDER BY DESC(?mass)
+    LIMIT 10
+    }
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+    }
+    """)
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print(result)
