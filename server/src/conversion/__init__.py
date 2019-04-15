fcb3b3f189711ab88fd3ffd8797298ee582081b6
Alexandre GISSAUD <gissaud.alexandre@gmail.com>
2019-04-15 15:37:22 +0900

    init new package conversion

---
+++ server/src/conversion/__init__.py
@@ -0,0 +1,14 @@
+# this package purpose is to convert natural language into
+#
+# In order to run, this package uses the following imports:
+#   - Wikipedia, the Python library:
+#           https://pypi.org/project/wikipedia/
+#   - Beautiful Soup, library used for pulling data out from HTML and XML files:
+#           https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+#   - SPARQLWrapper, Python wrapper around SPARQL (https://en.wikipedia.org/wiki/SPARQL) services:
+#           https://pypi.org/project/SPARQLWrapper/
+#
+#   All installation guides are described on their respective link.
+#
+
+
