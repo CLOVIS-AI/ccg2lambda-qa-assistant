5871ca59a20d5d0af69b5556c8007788f22e6bc2
Carpilieng <gamargueritte@gmail.com>
2019-04-15 22:33:23 +0900

    rectified test + changed query to go faster + wrapper returns a string

--- server/src/sparql/test_wrapper.py
+++ server/src/sparql/test_wrapper.py
@@ -7,4 +7,8 @@ class TestWrapper(TestCase):
         self.wrapper = Wrapper("https://query.wikidata.org/sparql", "query.sparql")
 
     def test_makeRequest(self):
-        self.wrapper.make_request()
+        expected_result = open("test_wrapper_expected_result.txt", "r")
+        actual_result = self.wrapper.make_request()
+        self.assertEqual(expected_result.read(), actual_result)
+        #print(actual_result)
+        expected_result.close()
--- server/src/sparql/wrapper.py
+++ server/src/sparql/wrapper.py
@@ -12,6 +12,7 @@ class Wrapper:
         self._wrapper = SPARQLWrapper(url)
         self.__query_path = query
 
+    # Creates the request of the query given in its parameter and sends it to the URL. Returns the result of the query.
     def make_request(self):
         file = open(self.__query_path, "r")
         self._wrapper.setQuery(file.read())
@@ -19,7 +20,10 @@ class Wrapper:
         self._wrapper.setReturnFormat(JSON)
         results = self._wrapper.query().convert()
 
+        string_result = ""
+        # Will end by a new empty line.
         for result in results["results"]["bindings"]:
-            print(result)
+            string_result += result["itemLabel"]["value"] + "\n"
 
         file.close()
+        return string_result
