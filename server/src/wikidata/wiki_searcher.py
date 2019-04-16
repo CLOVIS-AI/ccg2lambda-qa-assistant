087a0500433625e9f5735c6953dab153bc7dd4e9
Carpilieng <gamargueritte@gmail.com>
2019-04-16 17:58:12 +0900

    added (simple) function

--- server/src/wikidata/wiki_searcher.py
+++ server/src/wikidata/wiki_searcher.py
@@ -16,27 +16,28 @@ import warnings
 
 class WikiSearcher:
     def __init__(self, words):
-        self._words = words
+        self.__words = words
         # ignores warning from Wikipedia package about its use of bs4
         warnings.filterwarnings("ignore", category=UserWarning, module='wikipedia')
         # ignores warning of unclosed socket, existing only in unittest (hence suppressed to have a clear
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
-# returns url of wikipedia page resulting of a search on words var
+    # returns url of wikipedia page resulting of a search on words var
     def search_wikipedia(self):
         try:
-            page = wikipedia.page(self._words)
+            page = wikipedia.page(self.__words)
         except wikipedia.exceptions.DisambiguationError as e:
             page = wikipedia.page(e.options[0])
         except wikipedia.exceptions.PageError as e:
-            suggestion = wikipedia.suggest(self._words)
+            suggestion = wikipedia.suggest(self.__words)
             if suggestion is not None:
                 page = wikipedia.page(suggestion)
             else:
-                return "\"" + self._words + "\" does not match any pages. Try another id!"
+                return "\"" + self.__words + "\" does not match any pages. Try another id!"
 
         return page.url
 
+    # returns item q-code usable by wikidata (through SPARQL query)
     def search_wikidata_q_label(self):
         url = self.search_wikipedia()
         if url.startswith("\""):
@@ -51,5 +52,12 @@ class WikiSearcher:
 
             return q_number
 
+    # returns field p-code usable by wikidata (through SPARQL query)
+    def search_wikidata_p_field(self, dictionary):
+        if self.__words in dictionary:
+            return dictionary[self.__words]
+        else:
+            return None
+
     def set_words(self, words):
-        self._words = words
+        self.__words = words
