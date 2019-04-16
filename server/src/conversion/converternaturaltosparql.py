2801bd1b2eebfc22314a9798dcf9462e78453228
Alexandre GISSAUD <gissaud.alexandre@gmail.com>
2019-04-16 17:59:16 +0900

    ccg2lambda incorporated

--- server/src/conversion/converternaturaltosparql.py
+++ server/src/conversion/converternaturaltosparql.py
@@ -1,5 +1,6 @@
-import os
+import subprocess
 import sys
+import os
 
 PATH_TO_CCG2LAMBDA = "../../ccg2lambda"
 PATH_TO_CANDC = PATH_TO_CCG2LAMBDA + "/candc-1.00"
@@ -12,6 +13,28 @@ class ConverterNaturalToSPARQL:
     def __init__(self, sentences):
         self._sentences = sentences
 
+    def _settup_checker(self):
+        if not os.path.isdir(PATH_TO_CCG2LAMBDA):
+            print("ccg2lambda not found : Please install from : https://github.com/mynlp/ccg2lambda or follow the "
+                  "README.md on : https://github.com/CLOVIS-AI/ccg2lambda-qa-assistant")
+            sys.exit(1)
+
+        if not os.path.isdir(PATH_TO_TMP):
+            print("Creating temporary directory at :" + PATH_TO_TMP)
+            os.system("mkdir " + PATH_TO_TMP)
+
+    def _output_checker(self, returned_output, cmd):
+        if not returned_output == 0:
+            print("Error while converting Natural to SPARQL : " + cmd)
+            print("Code error : " + returned_output)
+            exit(1)
+
+    def _tokenizing(self):
+        cmd = "echo " + "\"" + self._sentences + "\"" + " | sed -f " + PATH_TO_CCG2LAMBDA + "/en/tokenizer.sed > " \
+              + PATH_TO_TMP + "/sentences.tok;"
+        returned_output = subprocess.call(cmd, shell=True)
+        self._output_checker(returned_output, cmd)
+
     def _candc_conversion(self):
         if not os.path.isdir(PATH_TO_CANDC):
             print("Error while using candc : directory not found at " + PATH_TO_CANDC)
@@ -19,34 +42,31 @@ class ConverterNaturalToSPARQL:
             exit(1)
 
         # Converting the sentences in Jigg's XML
-        cmd = "echo " + "\"" + self._sentences + "\"" + " | sed -f " + PATH_TO_CCG2LAMBDA + "/en/tokenizer.sed > " \
-              + PATH_TO_TMP + "/sentences.tok;"
-        os.system(cmd)
+        self._tokenizing()
 
-        cmd = PATH_TO_CANDC + "/bin/candc --models /path/to/candc-1.00/models --candc-printer xml --input " \
+        cmd = PATH_TO_CANDC + "/bin/candc --models " + PATH_TO_CANDC + "/models --candc-printer xml --input " \
               + PATH_TO_TMP + "/sentences.tok > " + PATH_TO_TMP + "/sentences.candc.xml"
-        os.system(cmd)
+        returned_output = subprocess.call(cmd, shell=True)
+        self._output_checker(returned_output, cmd)
 
-        cmd = "python" + PATH_TO_CCG2LAMBDA + "/en/candc2transccg.py + " + PATH_TO_TMP + "/sentences.candc.xml > " \
+        cmd = "python " + PATH_TO_CCG2LAMBDA + "/en/candc2transccg.py " + PATH_TO_TMP + "/sentences.candc.xml > " \
               + PATH_TO_TMP + "/sentences.xml"
-
-        os.system(cmd)
+        returned_output = subprocess.call(cmd, shell=True)
+        self._output_checker(returned_output, cmd)
 
     def _ccg2lambda_conversion(self):
         cmd = "python " + PATH_TO_CCG2LAMBDA + "/scripts/semparse.py " + PATH_TO_TMP + "/sentences.xml " \
               + TEMPLATE + " " + PATH_TO_TMP + "/sentences.sem.xml"
-        os.system(cmd)
+        returned_output = subprocess.call(cmd, shell=True)
+        self._output_checker(returned_output, cmd)
 
-    def Convert(self):
-        if not os.path.isdir(PATH_TO_CCG2LAMBDA):
-            print("ccg2lambda not found : Please install from : https://github.com/mynlp/ccg2lambda")
-            sys.exit(1)
+    def visualize_semantic(self):
+        cmd = "python " + PATH_TO_CCG2LAMBDA + "/scripts/visualize.py " + PATH_TO_TMP + "/sentences.sem.xml > " \
+              + PATH_TO_TMP + "/sentences.html"
+        returned_output = subprocess.call(cmd, shell=True)
+        self._output_checker(returned_output, cmd)
 
-        if not os.path.isdir(PATH_TO_TMP):
-            print("Creating temporary directory at :" + PATH_TO_TMP)
-            os.system("mkdir " + PATH_TO_TMP)
-
+    def convert(self):
         self._candc_conversion()
         self._ccg2lambda_conversion()
-
-
+        self.visualize_semantic()
--- server/src/conversion/test_converter.py
+++ server/src/conversion/test_converter.py
@@ -1,10 +1,20 @@
 from unittest import TestCase
 
-from .converternaturaltosparql import ConverterNaturalToSPARQL
+from .converternaturaltosparql import *
+
+from lxml import etree
+
 
 class TestWrapper(TestCase):
     def setUp(self):
-        self.converter = ConverterNaturalToSPARQL("What are the languages in China ?")
+        self.converter = ConverterNaturalToSPARQL("What are the languages in China ? \n Who is John Watson ? \
+                                                    \n How many dogs were in my house in 2014 ?")
 
     def test_makeRequest(self):
-        self.converter.Convert()
+        self.converter.convert()
+        self.converter.visualize_semantic()
+
+        parser = etree.XMLParser(ns_clean=True)
+        tree = etree.parse(PATH_TO_TMP + "/sentences.sem.xml", parser)
+
+        print(etree.tostring(tree, pretty_print=True))
