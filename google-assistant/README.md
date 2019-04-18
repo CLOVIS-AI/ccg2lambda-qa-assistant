5ab5d0990c5a612a4a65589f8e6eee027c774762
Ivan Canet <ivan.canet@gmail.com>
2019-04-18 16:51:26 +0900

    Creation of the IDEA project for the Google Assistant, along with boilerplate code & Gradle configuration

--- google-assistant/README.md
+++ google-assistant/README.md
@@ -1,3 +1,40 @@
 # ccg2lambda QA Assistant: Google Assistant API
 
 This directory contains the Google Assistant API link to the project.
+
+This directory is a heavily-modified copy of the [official repository](https://github.com/actions-on-google/actionssdk-say-number-java),
+which is legally allowed by the Apache 2 license that the repository is distributed under. The `LICENSE` file in this
+directory is the unmodified license of the original repository.
+
+## Installation
+
+### Prerequisites & configuration
+
+#### Action Console
+1. From the [Actions on Google Console](https://console.actions.google.com/), add a new project (this will become your *Project ID*) > **Create Project**.
+1. Scroll down > under **More options** select **Actions SDK** > keep **Use Actions SDK to add Actions** modal open
+
+#### GActions CLI
+
+1. [Install the gactions CLI](https://developers.google.com/actions/tools/gactions-cli).
+
+#### GCloud
+
+1. Install the [Google Cloud SDK](https://cloud.google.com/sdk/docs/)
+1. Setup the SDK:
+    + Run `gcloud auth application-default login` with your Gooogle account
+    + Install and update the App Engine component,`gcloud components install app-engine-java`
+    + Update other components, `gcloud components update`
+
+#### App Engine Deployment
+
+1. Configure the GCloud: `gcloud init`
+1. Deploy:
+    + `cd google-assistant`
+    + `gradle appEngineDeploy`
+1. Open the `action.json` file:
+   + If it is missing, copy it from [the official repository](https://github.com/actions-on-google/actionssdk-say-number-java/blob/master/action.json).
+   + In the **conversations object** > replace the placeholder **URL** values with `https://<YOUR_PROJECT_ID>.appspot.com`
+1. In terminal, run `gactions update --action_package action.json --project <YOUR_PROJECT_ID>`
+1. Back in the [Actions console](https://console.actions.google.com), from the **Use Actions SDK to add Actions** window > select **OK**.
+1. From the left menu under **Test** > select **Simulator** to open the Actions on Google simulator then say or type `Talk to my test app`.
---
+++ google-assistant/src/main/java/com/example/SayNumberApp.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.google.actions.api.ActionRequest;
+import com.google.actions.api.ActionResponse;
+import com.google.actions.api.ActionsSdkApp;
+import com.google.actions.api.ForIntent;
+import com.google.actions.api.response.ResponseBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+// Libraries for Google Assistant SDK
+
+public class SayNumberApp extends ActionsSdkApp {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(SayNumberApp.class);
+
+    // Note: Do not store any state as an instance variable.
+    // It is ok to have final variables where the variable is assigned a value in
+    // the constructor but remains unchanged. This is required to ensure thread-
+    // safety as the entry point (ActionServlet) instances may
+    // be reused by the server.
+
+    @ForIntent("actions.intent.MAIN")
+    public ActionResponse welcome(ActionRequest request) {
+        LOGGER.info("Welcome intent start.");
+        ResponseBuilder responseBuilder = getResponseBuilder(request);
+        ResourceBundle rb = ResourceBundle.getBundle("resources",
+                request.getLocale());
+        responseBuilder.add(rb.getString("welcome"));
+
+        LOGGER.info("Welcome intent end.");
+        return responseBuilder.build();
+    }
+
+    @ForIntent("actions.intent.TEXT")
+    public ActionResponse number(ActionRequest request) {
+        LOGGER.info("Number intent start.");
+        ResponseBuilder responseBuilder = getResponseBuilder(request);
+        ResourceBundle rb = ResourceBundle.getBundle("resources");
+
+        String userNumber = request.getArgument("text").getTextValue();
+        String response;
+        if (userNumber.equals("bye")) {
+            response = rb.getString("bye");
+            responseBuilder.add(response).endConversation();
+        } else {
+            response = MessageFormat.format(rb.getString("sayOrdinal"), userNumber);
+            responseBuilder.add(response);
+        }
+        LOGGER.info("Number intent end.");
+        return responseBuilder.build();
+    }
+
+
+}
