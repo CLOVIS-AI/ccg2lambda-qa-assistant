339fea5cae9796d7b0ae095fa967fa2f160f202c
Carpilieng <gamargueritte@gmail.com>
2019-04-18 17:26:42 +0900

    rectified path problem

--- server/src/wikidata/test_dictionary.py
+++ server/src/wikidata/test_dictionary.py
@@ -5,11 +5,11 @@ from wikidata.properties_dictionary import Dictionary
 
 class TestDictionary(TestCase):
     def test_fill_dictionary(self):
-        dictionary = Dictionary("../../res/HTML")
+        dictionary = Dictionary("../res/HTML")
         dictionary.fill_dictionary()
 
     def test_load_dictionary(self):
-        dictionary = Dictionary("res/HTML")
+        dictionary = Dictionary("../res/HTML")
         assert dictionary.get_dictionary() == {}
         dictionary.load_dictionary()
         assert dictionary.get_dictionary() != {}
