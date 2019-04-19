3d31b3d2556cd8cbb6d73b8bc6c76ee230631338
Carpilieng <gamargueritte@gmail.com>
2019-04-19 13:57:26 +0900

    added synonym support at dictionary.

--- server/src/wikidata/test_dictionary.py
+++ server/src/wikidata/test_dictionary.py
@@ -5,10 +5,16 @@ from wikidata.properties_dictionary import Dictionary
 
 class TestDictionary(TestCase):
     def test_fill_dictionary(self):
+        """
+        Must not be run from this file. Run instead from the global test section for dynamic paths reasons.
+        """
         dictionary = Dictionary("../res/HTML")
         dictionary.fill_dictionary()
 
     def test_load_dictionary(self):
+        """
+        Must not be run from this file. Run instead from the global test section for dynamic paths reasons.
+        """
         dictionary = Dictionary("../res/HTML")
         assert dictionary.__getitem__("instance of") is None
         dictionary.load_dictionary()
