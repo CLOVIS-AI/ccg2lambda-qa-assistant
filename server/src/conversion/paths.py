fff181eac59edc1e1f1b4fcbc47fbbc702788242
Ivan Canet <ivan.canet@gmail.com>
2019-04-19 18:58:07 +0900

    Find the template file

--- server/src/conversion/paths.py
+++ server/src/conversion/paths.py
@@ -32,7 +32,7 @@ def test_file(path):
 
 
 def init_paths():
-    global PATHS_READY, PATH_TO_CANDC, PATH_TO_CCG2LAMBDA
+    global PATHS_READY, PATH_TO_CANDC, PATH_TO_CCG2LAMBDA, TEMPLATE
     if PATHS_READY:
         print("Paths are already ready; exiting init_paths function")
         return
@@ -49,6 +49,12 @@ def init_paths():
             print(" › C&C is not installed")
             raise Exception("Couldn't find C&C. Since you installed ccg2lambda as a submodule, "
                             "check that you installed C&C correctly according to ccg2lambda's documentation.")
+
+        if test_file(TEMPLATE):
+            print(" › Found the template file")
+        else:
+            print(" › Template file not found")
+            raise Exception("Couldn't find the template file.")
     else:
         print(" › ccg2lambda is not installed as a submodule")
 
@@ -65,6 +71,13 @@ def init_paths():
         else:
             print(" › C&C is not installed")
             raise Exception("Couldn't find C&C. Expected to find it in the Docker container.")
+
+        if test_file(PATH_TO_CCG2LAMBDA + "/en/semantic_templates_en_event.yaml"):
+            print(" › Template found")
+            TEMPLATE = PATH_TO_CCG2LAMBDA + "/en/semantic_templates_en_event.yaml"
+        else:
+            print(" › Template not found")
+            raise Exception("Couldn't find the template.")
     print("Done initializing.\n")
     PATHS_READY = True
     exit(1)
