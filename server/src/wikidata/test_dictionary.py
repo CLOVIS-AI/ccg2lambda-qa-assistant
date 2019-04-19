3e71f999353157871c0c2518cb311494290a4896
Carpilieng <gamargueritte@gmail.com>
2019-04-19 18:51:26 +0900

    added some tests

--- server/src/wikidata/test_dictionary.py
+++ server/src/wikidata/test_dictionary.py
@@ -14,8 +14,15 @@ class TestDictionary(TestCase):
     def test_load_dictionary(self):
         """
         Must not be run from this file. Run instead from the global test section for dynamic paths reasons.
+        Considers that a dictionary exists
         """
         dictionary = Dictionary("../res/HTML")
         assert dictionary.__getitem__("instance of") is None
         dictionary.load_dictionary()
         assert dictionary.__getitem__("instance of") == "P31"
+        assert dictionary.__getitem__("discography") == "P358"
+
+        # with synonyms
+        assert dictionary.__getitem__("neighborhood") == "P276"
+        assert dictionary.__getitem__("location") == "P276"
+        assert dictionary.__getitem__("place held") == "P276"
