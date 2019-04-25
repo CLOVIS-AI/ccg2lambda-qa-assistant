d25c3230e3b3cdaf75f3e5028ea238077d35e375
Ivan Canet <ivan.canet@gmail.com>
2019-04-25 18:25:44 +0900

    Paths to find RTE & parser_location.txt in ccg2lambda

--- server/src/conversion/paths.py
+++ server/src/conversion/paths.py
@@ -20,6 +20,7 @@ DEPCCG = PROJECT_ROOT + "depccg"
 TMP = "tmp"
 TEMPLATE = "../res/parser/semantic_templates_en_qa.yaml"
 RTE = ""
+PARSER_LOCATION_FILE = ""
 PATHS_READY = False
 
 
@@ -64,7 +65,7 @@ def init_paths():
     or reports if they are missing.
     Searches in the submodule or in a Docker container.
     """
-    global PATHS_READY, CANDC, CCG2LAMBDA, TEMPLATE, RTE
+    global PATHS_READY, CANDC, CCG2LAMBDA, TEMPLATE, RTE, PARSER_LOCATION_FILE
     if PATHS_READY:
         print("Paths are already ready; exiting init_paths function")
         return
@@ -117,12 +118,24 @@ def init_paths():
         os.mkdir(TMP)
         print(" Done.")
 
-    RTE = SERVER_ROOT + "res/parser/rte_en_qa.sh"
+    RTE = CCG2LAMBDA + "/en/rte_en_qa.sh"
     if test_file(RTE):
         print(" › Found the rte_en_qa script")
     else:
         print(" › rte_en_qa not found")
         raise Exception("Couldn't find the rte_en_qa script.")
+
+    PARSER_LOCATION_FILE = CCG2LAMBDA + "/en/parser_location.txt"
+    print("Writing depccg's location to [", os.path.abspath(PARSER_LOCATION_FILE), "]")
+
+    parser_location = open(PARSER_LOCATION_FILE, "w+")
+    parser_location.write("depccg:" + os.path.abspath(DEPCCG))
+    parser_location.close()
+    if test_file(PARSER_LOCATION_FILE):
+        print(" › Successfully created the parser_location file")
+    else:
+        print(" › parser_location not found, but I just created it!")
+        raise Exception("Couldn't find the parser_location file.")
 
     print("Done initializing.\n")
     PATHS_READY = True
