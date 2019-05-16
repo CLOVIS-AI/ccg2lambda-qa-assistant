37de2ea8acb47ed152ce3a4fb5a388ae24128a05
Ivan Canet <ivan.canet@gmail.com>
2019-05-16 16:08:50 +0900

    Creation of a public interface for the nltk2qo package

---
+++ server/src/nltk2qo/__init__.py
@@ -0,0 +1,8 @@
+###########################################################
+#                                                         #
+#      Handles the conversion from NTLK-parsed            #
+#      objects to objects that can easily be used         #
+#      to create SPARQL queries, which we will            #
+#      call 'Query Objects' (QO) in this package.         #
+#                                                         #
+###########################################################
