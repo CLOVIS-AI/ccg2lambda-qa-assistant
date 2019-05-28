382f9c84a7cb2e64c13ea703469b27ddf02f4aef
Carpilieng <gamargueritte@gmail.com>
2019-05-28 15:35:34 +0900

    removed unused function in wrapper and added query template for query builder

--- server/src/sparql/wrapper.py
+++ server/src/sparql/wrapper.py
@@ -30,27 +30,3 @@ class Wrapper:
 
         file.close()
         return string_result
-
-    def make_request_dictionary(self):
-        """
-        Wrapper designed in order to fill the dictionary, as its return values used are different from usual requests.
-        :return: result of the query
-        """
-        file = open(self._query_path, "r")
-        self._wrapper.setQuery(file.read())
-
-        self._wrapper.setReturnFormat(JSON)
-        results = self._wrapper.query().convert()
-
-        string_result = ""
-        # Will end by a new empty line
-        for result in results["results"]["bindings"]:
-            p_code = result["property"]["value"].split('/')[-1]
-            string_result += result["propertyLabel"]["value"] + \
-                ':' + p_code + "\n"
-            if "propertyAltLabel" in result:
-                for alias in result["propertyAltLabel"]["value"].split(", "):
-                    string_result += alias + ':' + p_code + "\n"
-
-        file.close()
-        return string_result
