dd03e4ef1ef718faab7465e6d138a0615b9d0439
Carpilieng <gamargueritte@gmail.com>
2019-05-28 15:44:39 +0900

    rectified wrong use of file in wrapper

--- server/src/sparql/wrapper.py
+++ server/src/sparql/wrapper.py
@@ -8,25 +8,23 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 class Wrapper:
 
-    def __init__(self, url, query_path):
-        self._wrapper = SPARQLWrapper(url)
-        self._query_path = query_path
+    def __init__(self, url, query):
+        self.__wrapper = SPARQLWrapper(url)
+        self.__query = query
 
     def make_request(self):
         """
         Creates the request of the query given in its parameter and sends it to the URL.
         :return: result of the query
         """
-        file = open(self._query_path, "r")
-        self._wrapper.setQuery(file.read())
+        self.__wrapper.setQuery(self.__query)
 
-        self._wrapper.setReturnFormat(JSON)
-        results = self._wrapper.query().convert()
+        self.__wrapper.setReturnFormat(JSON)
+        results = self.__wrapper.query().convert()
 
         string_result = ""
         # Will end by a new empty line.
         for result in results["results"]["bindings"]:
             string_result += result["itemLabel"]["value"] + "\n"
 
-        file.close()
         return string_result
